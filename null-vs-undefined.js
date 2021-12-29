f202748cacefacb9bcbd8c37a7a64dd4ec8b2896
BILLAHMASUM325 <masumbillah3.25@outlook.com>
2021-12-29 20:38:58 +0600

    practice

---
+++ null-vs-undefined.js
@@ -0,0 +1,12 @@
+// parameter na dela
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(12,34);
+// console.log(result);
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const premik = {name:"Billah", phone: 4533555};
+console.log(premik.id)
